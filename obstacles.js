55c25cdb24580e1b482c393fefe8dfd00fb6b410
rebeca1012 <rebeca.ledesma@gmail.com>
2024-11-23 19:33:11 -0400

    modified textures

--- obstacles.js
+++ obstacles.js
@@ -44,21 +44,57 @@ export function createObstacleBox(position, size) {
       16, 17, 18, 16, 18, 19, // right
       20, 21, 22, 20, 22, 23  // left
     ]);
+
+    // uv coords
+    const uvs = new Float32Array([
+      0, 0,  1, 0,  1, 1,  0, 1, // front
+      0, 0,  1, 0,  1, 1,  0, 1, // back
+      0, 0,  1, 0,  1, 1,  0, 1, // top
+      0, 0,  1, 0,  1, 1,  0, 1, // bottom
+      0, 0,  1, 0,  1, 1,  0, 1, // right
+      0, 0,  1, 0,  1, 1,  0, 1  // left
+    ]);
   
     geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setAttribute('uv', new THREE.BufferAttribute(uvs, 2));
     geometry.setIndex(new THREE.BufferAttribute(indices, 1));
     geometry.computeVertexNormals();
-  
+
+    // Create TextureLoader
     const textureLoader = new THREE.TextureLoader();
-    const woodTexture = textureLoader.load('../static/wood.png');
-    const material = new THREE.MeshStandardMaterial({ map: woodTexture });
+    
+    // Load all textures
+    const colorMap = textureLoader.load('../static/Wood049_1K-JPG_Color.jpg');
+    const normalMap = textureLoader.load('../static/Wood049_1K-JPG_NormalGL.jpg');
+    const roughnessMap = textureLoader.load('../static/Wood049_1K-JPG_Roughness.jpg');
+    const displacementMap = textureLoader.load('../static/Wood049_1K-JPG_Displacement.jpg');
+    
+    // configure texture and repeat it
+    const textures = [colorMap, normalMap, roughnessMap, displacementMap];
+    textures.forEach(texture => {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.set(2, 2);
+    });
+
+    // Create material 
+    const material = new THREE.MeshStandardMaterial({
+        map: colorMap,
+        normalMap: normalMap,
+        roughnessMap: roughnessMap,
+        displacementMap: displacementMap,
+        displacementScale: 0.004, 
+        normalScale: new THREE.Vector2(1, 1)
+    });
+
     const box = new THREE.Mesh(geometry, material);
     box.position.copy(position);
+    box.castShadow = true;
+    box.receiveShadow = true;
 
-	const boundingBox = new THREE.Box3().setFromObject(box);
-	box.userData.boundingBox = boundingBox;
+    const boundingBox = new THREE.Box3().setFromObject(box);
+    box.userData.boundingBox = boundingBox;
     return box;
-  }
+}
 
 // Function to create circular target
 export function createCircularTarget(position, radius, depth) {
@@ -134,8 +170,9 @@ export function createCircularTarget(position, radius, depth) {
   
     group.position.copy(position);
 
-	const boundingBox = new THREE.Box3().setFromObject(group);
-	group.userData.boundingBox = boundingBox;
+    const boundingBox = new THREE.Box3().setFromObject(group);
+    group.userData.boundingBox = boundingBox;
 
     return group;
-  }
+}
+
